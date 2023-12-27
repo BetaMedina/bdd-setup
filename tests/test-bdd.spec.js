7713ed86bb989469ecf1132c3cfec224f94c5de4
medina <joaopedromedinadeandrade@gmail.com>
2023-12-27 13:38:23 -0300

    feat: adding a fail case

--- tests/test-bdd.spec.js
+++ tests/test-bdd.spec.js
@@ -4,26 +4,36 @@ function passwordValidator() {
     setPassword: (newPassword) => {
       this.password = newPassword;
     },
-    validatePassword: (passwordToValidate) => this.password === passwordToValidate
+    validatePassword: (passwordToValidate) => {
+      return this.password === passwordToValidate
+    }
   }
 }
+let sut = passwordValidator();
 describe('Feature: Validate password', () => {
-  let sut = passwordValidator();
-  let accessGranted = false;
-
   beforeEach(() => {
     sut = passwordValidator();
   });
-
   scenario('Entering a correct password', () => {
     given('I have previously created a password', () => {
       sut.setPassword('1234');
 
       when('I enter my password correctly', () => {
-        accessGranted = sut.validatePassword('1234');
+        const accessGranted = sut.validatePassword('1234');
 
         then('I should be granted access', () => {
           expect(accessGranted).toBeTruthy();
+        });
+      });
+    });
+    given("I haven't previously created a password", () => {
+      sut.setPassword('12345');
+
+      when('I enter my inexistent password', () => {
+        const accessGranted = sut.validatePassword('1234');
+
+        then('I should be blocked', () => {
+          expect(accessGranted).toBeFalsy();
         });
       });
     });
